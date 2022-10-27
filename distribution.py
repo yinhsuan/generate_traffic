aa0c3aa4a182b14d81626d3f759095eebf54ecd1
yinhsuan <sandy10235168@gmail.com>
2022-10-27 16:38:36 +0800

    test normal distribution

---
+++ distribution.py
@@ -0,0 +1,27 @@
+import subprocess as sp
+import sys
+import numpy as np
+
+def normal_distribution(mean, std, size):
+    return np.random.normal(loc = mean, scale = std, size = size)
+
+def generate_traffic():
+    # Load
+    mean = 20
+    std = 0.1
+    size = 1
+
+    load = normal_distribution(mean, std, size)
+    print("{load}".format(load=load))
+
+    # Flow Duration
+
+    # Racks Size
+
+    # Server
+
+
+
+
+if __name__ == "__main__":
+    generate_traffic()
