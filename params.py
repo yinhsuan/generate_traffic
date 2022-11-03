f8be997006f233f096a224eb033a9670e5809a8d
yinhsuan <sandy10235168@gmail.com>
2022-11-03 17:19:10 +0800

    edit parameter

--- params.py
+++ params.py
@@ -16,10 +16,13 @@ START_PORT = 7000
 # User Parameters
 CASE = "locality" # <fixed>/ <whole>/ <locality>
 TRAFFIC_FILE = "tmp.txt"
-FLOW_MEAN = 0.05
-FLOW_STD = 0.03
-SCALE = 100.0 # number MUST larger than 0
-DST_RACK_MEAN = 8.0 # if user do not want to define => set: 0.0
-DST_RACK_STD = 2.0
 PROTOCOL = "u"
 IPERF_VERSION = "iperf"
+
+FLOW_MEAN = 0.05
+FLOW_STD = 0.03
+
+DST_RACK_MEAN = 8.0 # if user do not want to define => set: 0.0
+DST_RACK_STD = 2.0
+
+SCALE = 100.0 # number MUST larger than 0
